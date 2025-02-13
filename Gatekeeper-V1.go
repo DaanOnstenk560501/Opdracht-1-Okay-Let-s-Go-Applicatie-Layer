e199cce427be0b08a7726c4b6647dd267390682a
Daan Onstenk 560501 <560501@student.fontys.nl>
2025-02-13 13:55:54 +0100

    Aanpassende groet n.a.v dagdeel toegevoegd

--- Gatekeeper-V1.go
+++ Gatekeeper-V1.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+)
 
 func main() {
-    fmt.Println("hello world")
+    HuidigeTijd := time.Now().Hour()
+    var groet string
+
+    switch {
+    case HuidigeTijd >= 7 && HuidigeTijd < 12:
+        groet = "Goedemorgen"
+    case HuidigeTijd >= 12 && HuidigeTijd < 18:
+        groet = "Goedemiddag"
+    case HuidigeTijd >= 18 && HuidigeTijd < 23:
+        groet = "Goedenavond"
+    default:
+        fmt.Println("Sorry, de parkeerplaats is 's nachts gesloten")
+        return
+    }
+
+    fmt.Println(groet + "! Welkom bij Fonteyn Vakantieparken")
+
 }
