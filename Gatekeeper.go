968e4f620e37b89f82a1859ce3915e395367ad7e
Daan Onstenk 560501 <560501@student.fontys.nl>
2025-02-20 08:55:55 +0100

    .mod uit Gitignore verwijderd n.a.v feedback van docent

--- Gatekeeper.go
+++ Gatekeeper.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "time"
+	"fmt"
+	"strings"
+	"time"
 )
 
 func main() {
-    var kenteken string
-    fmt.Print("Voer uw kenteken in: ")
-    fmt.Scanln(&kenteken)
+	var kenteken string
+	fmt.Print("Voer uw kenteken in: ")
+	fmt.Scanln(&kenteken)
 
-    kenteken = strings.ToUpper(kenteken)
+	kenteken = strings.ToUpper(kenteken)
 
-    toegestaneKentekens := map[string]bool{
-        "ABC123": true,
-        "XYZ789": true,
-        "LMN456": true,
-    }
+	toegestaneKentekens := map[string]bool{
+		"ABC123": true,
+		"XYZ789": true,
+		"LMN456": true,
+	}
 
-    if !toegestaneKentekens[kenteken] {
-        fmt.Println("U heeft helaas geen toegang tot het parkeerterrein")
-        fmt.Println("Druk op Enter om af te sluiten...")
-        fmt.Scanln()
-        return
-    }
+	if !toegestaneKentekens[kenteken] {
+		fmt.Println("U heeft helaas geen toegang tot het parkeerterrein")
+		fmt.Println("Druk op Enter om af te sluiten...")
+		fmt.Scanln()
+		return
+	}
 
-    HuidigeTijd := time.Now().Hour()
-    var groet string
+	HuidigeTijd := time.Now().Hour()
+	var groet string
 
-    switch {
-    case HuidigeTijd >= 7 && HuidigeTijd < 12:
-        groet = "Goedemorgen"
-    case HuidigeTijd >= 12 && HuidigeTijd < 18:
-        groet = "Goedemiddag"
-    case HuidigeTijd >= 18 && HuidigeTijd < 23:
-        groet = "Goedenavond"
-    default:
-        fmt.Println("Sorry, de parkeerplaats is 's nachts gesloten")
-        fmt.Println("Druk op Enter om af te sluiten...")
-        fmt.Scanln()
-        return
-    }
+	switch {
+	case HuidigeTijd >= 7 && HuidigeTijd < 12:
+		groet = "Goedemorgen"
+	case HuidigeTijd >= 12 && HuidigeTijd < 18:
+		groet = "Goedemiddag"
+	case HuidigeTijd >= 18 && HuidigeTijd < 23:
+		groet = "Goedenavond"
+	default:
+		fmt.Println("Sorry, de parkeerplaats is 's nachts gesloten")
+		fmt.Println("Druk op Enter om af te sluiten...")
+		fmt.Scanln()
+		return
+	}
 
-    fmt.Println(groet + "! Welkom bij Fonteyn Vakantieparken")
-    fmt.Println("Druk op Enter om af te sluiten...")
-    fmt.Scanln()
+	fmt.Println(groet + "! Welkom bij Fonteyn Vakantieparken")
+	fmt.Println("Druk op Enter om af te sluiten...")
+	fmt.Scanln()
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Gatekeeper-V1
+
+go 1.24.0
